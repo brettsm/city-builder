cb800b540a6d3b44e3e799a742ac6871e7b1ff39
Brett Smith <brettdansmith@gmail.com>
2023-07-12 21:41:53 -0400

    Build basic framework for how game will operate Add .json files for c++ compiler Add .gitignore file to ignore .dSYM

---
+++ city_builder.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    bool isRunning = true;
+
+    //initialization function
+    //initializeCity();
+        //Each saved game will be referred to as a "City"
+    
+    std::cout << "Setting things up";
+
+    while(isRunning) {
+        //Process input func
+
+
+        //Update game state func
+
+
+        //Render the game func
+        //This step involves rendering the current game state and displaying it on the screen. 
+        //It includes drawing the game world, characters, objects, UI elements, and any other visual components. 
+        //This process often utilizes graphics libraries to handle rendering operations efficiently.
+    }
+
+    //Cleanup func
+
+    return 1;
+}
