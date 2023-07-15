d70937a2b7dcb7f7c6ae3968ff63e6ada51e3fa9
Brett Smith <brettdansmith@gmail.com>
2023-07-15 16:27:27 -0400

    Flesh out documentation in city_builder.cpp Create building.cpp for 'building' objects

---
+++ building.cpp
@@ -0,0 +1,33 @@
+class building {
+    private:
+        int numResidents;
+        int numEmptyRes;
+        int dateBuilt;
+        int price;
+        char* buildingType;
+        bool nearRoad = false;
+        
+    //     //probably need something to hold the image that will be dislpayed
+    public:
+        building(char* zone) {
+            if(zone == "Residential") {
+                //some sort of thing to decide if the residential is high density or low density housing
+                    //maybe there should be another function for this? (Probably not)
+
+            } else if(zone == "Shops") {
+                buildingType = "Shop";
+                //figure out how to set date
+                    //maybe a time class to keep track of and return in game time to funcs that need it
+            } else {
+                buildingType = "Farm";
+            }
+        }
+        void checkIfRoad() {
+            //check if there is a 'road' on any side of the building and if that road is connected
+            //if there is road and it is conencted to the main roads
+            //then the building can start being used/populated
+                //update 
+
+            //if there 
+        }
+};
--- city_builder.cpp
+++ city_builder.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main(int argc, char* argv[]) {
     bool isRunning = true;
