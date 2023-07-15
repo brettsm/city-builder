c796c82c600aad90c43e07777f8111b38ab286b8
Brett Smith <brettdansmith@gmail.com>
2023-07-15 17:48:25 -0400

    change rand.cpp to stats_city.cpp add functionality for randomly assigning type of building next_exponent function to return poissan for citizen activity

---
+++ stats_city.cpp
@@ -0,0 +1,118 @@
+#include <random>
+#include <string>
+
+class stats_city {
+    
+    //building distribution:
+    //  Residential:
+    //      80% high density
+    //      20% low density
+    //  Shops: (could find a way later to decide by necessity in area)
+    //      40% Food Stores
+    //          20% Butcher
+    //          80% Produce Stand
+    //      10% Alternative Medicine Stores (potions, herbs, crystals etc.)
+    //      20% General Goods Store (hardware, etc)
+    //      20% Book Store
+    //      10% Spell Store (wizard)
+    //  Farm:
+    //      10% Apple Tree Farm
+    //      30% Potato Farm
+    //      20% Corn Farm
+    //      40% Wheat Farm
+
+    //where it says randFloat = 0, replace with whatever the random function is
+    public:
+        char* choose_building_residential() {
+            float randFloat = 0; //randomFloatFunction in C++
+            if(randFloat <= .2) {
+                return "low";
+            } else {
+                return "high";
+            }
+        }
+        char* choose_building_shop() {
+            float randFloat = 0;
+            if(randFloat <= .1) {
+                return "medicine";
+            } else if(randFloat <= .2) {
+                return "spell";
+            } else if(randFloat <= .4) {
+                return "general";
+            } else if(randFloat <= .6) {
+                return "book";
+            } else {
+                randFloat = 0;
+                if(randFloat <= .2) {
+                    return "butcher";
+                } else {
+                    return "produce";
+                }
+            }
+        }
+        char* choose_building_farm() {
+            float randFloat = 0;
+            if(randFloat <= .1) {
+                return "apple";
+            } else if(randFloat <= .3) {
+                return "corn";
+            } else if(randFloat <= .6) {
+                return "potato";
+            } else {
+                return "wheat";
+            }
+        }
+        float next_exponential() {
+            float x;
+            if(x == 0.0) {
+                return 0.0;
+            } else if(x <= 0.1) {
+                return ((x - 0.00) * 1.04 + 0.0);
+            } else if(x <= 0.2) {
+                return ((x - 0.10) * 1.18 + 0.104);
+            } else if(x <= 0.3) {
+                return ((x - 0.20) * 1.33 + 0.222);
+            } else if(x <= 0.4) {
+                return ((x - 0.30) * 1.54 + 0.355);
+            } else if(x <= 0.5) {
+                return ((x - 0.40) * 1.81 + 0.509);
+            } else if(x <= 0.6) {
+                return ((x - 0.50) * 2.25 + 0.690);
+            } else if(x <= 0.7) {
+                return ((x - 0.60) * 2.85 + 0.915);
+            } else if(x <= 0.75) {
+                return ((x - 0.70) * 3.60 + 1.20);
+            } else if(x <= 0.80) {
+                return ((x - 0.75) * 4.40 + 1.38);
+            } else if(x <= 0.84) {
+                return ((x - 0.80) * 5.57 + 1.60);
+            } else if(x <= 0.88) {
+                return ((x - 0.84) * 7.25 + 1.83);
+            } else if(x <= 0.90) {
+                return ((x - 0.88) * 9.00 + 2.12);
+            } else if(x <= 0.92) {
+                return ((x - 0.90) * 11.0 + 2.30);
+            } else if(x <= 0.94) {
+                return ((x - 0.92) * 14.5 + 2.52);
+            } else if(x <= 0.95) {
+                return ((x - 0.94) * 18.0 + 2.81);
+            } else if(x <= 0.96) {
+                return ((x - 0.95) * 30.0 + 2.99);
+            } else if(x <= 0.97) {
+                return ((x - 0.96) * 30.0 + 3.20);
+            } else if(x <= 0.98) {
+                return ((x - 0.97) * 40.0 + 3.50);
+            } else if(x <= 0.99) {
+                return ((x - 0.98) * 70.0 + 3.90);
+            } else if(x <= 0.995) {
+                return ((x - 0.99) * 140.0 + 4.60);
+            } else if(x <= 0.998) {
+                return ((x - 0.995) * 300.0 + 5.30);
+            } else if(x <= 0.999) {
+                return ((x - 0.998) * 800.0 + 6.20);
+            } else {
+                return ((x - 0.9997) * 1000.0 + 8.0);
+            }
+        }
+        
+};
